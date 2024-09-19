99baede0cdff1e12ece100fa2ca3f18018957dd7
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-09-19 07:15:42 +0500

    bringup super ior os

--- aosp_a71.mk
+++ aosp_a71.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/samsung/a71/device.mk)
 
 # Inherit some common PixelBuilds stuff.
-$(call inherit-product, vendor/pb/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_a71
