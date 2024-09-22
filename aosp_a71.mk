e065b376a3af869a9594a47a61808191863a11e9
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-09-22 10:52:17 +0500

    a71: Revest SuperiorOS bringup

--- aosp_a71.mk
+++ aosp_a71.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/samsung/a71/device.mk)
 
 # Inherit some common PixelBuilds stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_a71
